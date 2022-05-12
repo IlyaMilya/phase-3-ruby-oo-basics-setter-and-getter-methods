699338fc3fd5d9b1cc2a89e8cb6396f707466260
Ilya <milyavskyfamily@gmail.com>
2022-05-11 21:39:45 -0400

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
 class Dog
 
+    def name=(name)
+        @name = name 
+    end
+
+    def name 
+        @name
+    end
+
+    fido = Dog.new
+    fido.name = ( "Fido" )
+    puts fido.name
+
+    def breed=(breed)
+        @breed = breed 
+    end 
+
+    def breed
+    @breed
+    end
+
+    breeder= Dog.new
+    breeder.breed = ("Yorkie")
+    puts breeder.breed 
 end
+
+
+
