06e0f7a4b64a26a392b8095ac471411a225e659b
Ilya <milyavskyfamily@gmail.com>
2022-05-11 21:39:33 -0400

    commit tree 898ba2e441983a1b7b684071c032f92661ff8977

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,29 @@
 class Person
 
+
+def name=(name)
+    @name= name
+end
+
+def name 
+    @name 
+end
+
+goodPerson = Person.new
+goodPerson.name = ("Ilya")
+puts goodPerson.name
+
+
+def job=(job)
+    @job = job
+end
+
+def job 
+    @job
+end
+
+professor = Person.new
+professor.job =('software engineer')
+puts professor.job
+
 end
